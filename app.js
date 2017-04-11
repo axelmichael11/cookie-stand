a549b7c2e975fd1ac26241ffb086198ef096dfff
Michael Axelson <axelmichael11@gmail.com>
2017-04-11 00:38:20 -0700

    my loop is doing something funky, added the list item part

--- app.js
+++ app.js
@@ -38,31 +38,43 @@ Within the product objects
 
 //1.
 
-var store_hours={
-  times:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm']
-}
-console.log(store_hours.length);
 
-var FIRSTANDPIKE = document.getElementById('title');
+var FIRSTANDPIKEUL = document.getElementById('first and pike');
+var SEATACAIRPORT = document.getElementById('SeaTac Airport');
+var SEATTLECENTER = document.getElementById('Seattle Center');
+var CAPITOLHILL = document.getElementById('Capitol Hill');
+var ALKI = document.getElementById('Alki');
 
 var first_and_pike = {
   min: 23,
   max: 65,
   avgCookies: 6.3,
-  hours: 0,
+  totalCookiesSold: 0,
   randomCustomers: function() {
   return Math.floor(Math.random()*(this.max-this.min+1)+this.min);
 },
-avgCookiesPerHr: function() {
+randomCookies: function() {
   return Math.round(this.randomCustomers()*this.avgCookies);
 },
-results: [this.min, this.max, this.avgCookies,this.randomCustomers(), this.avgCookiesPerHr()],
+//results: [this.min,this.max,this.avgCookies,this.randomCustomers(), this.avgCookiesPerHr()],
 store_hours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
-listCreation: function(){
-for (var i=0; i< 15; i++) {
+random_cookies_hr:
+[randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies(),randomCookies()]
+
+//listCreation: function(){
+//for (var i=0; i< 15; i++) {
+  var results=[];
+  //store_hours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+  results.push(this.store_hours[i]+":"+this.randomCustomers());
   console.log(this.store_hours[i]);
+  //ALRIGHT... I have hit a roadblock... I want to write BOTH my functions I used before! like this.... this.store_hours[i]+":"+this.avgCookiesPerHr...But I cant! What is "this" object changing and why have I been able to use it before...
+  //var listItem = document.createElement('li');
+  //listItem.textContent= results[i];
+  //FIRSTANDPIKEUL.appendChild(listItem);
+}
 }
 }
 
-
-console.log(first_and_pike.avgCookiesPerHr());
+console.log(first_and_pike.random_cookies_hr);
+console.log(first_and_pike.randomCustomers());
+console.log(first_and_pike.randomCookies());

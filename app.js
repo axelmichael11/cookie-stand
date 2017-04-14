6a05fc98a9c76ad36abdc507f52f559d99de6b9d
Michael Axelson <axelmichael11@gmail.com>
2017-04-13 17:52:16 -0700

    final version

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
 'use strict';
-/*
-I am going to organize my salmon cookie shop's products... create a products' array full of product objects...
-
-Within the product objects
-  cookies
-    cookieName
-    bake_date
-    Price
-    Ingredients
-    Description
-  brownies
-    brownieName
-    Price
-    Ingredients
-    Description
-  smoothies
-    smoothieName
-    Price
-    Ingredients
-    Description
-*/
-
-//
-// lemon.cookieName = 'salmon lemon biscuit';
-// lemon.bake_date ='4/10/17';
-// lemon.price = 4.99;
-// lemon.ingredients = ['salmon','lemons','eggs'];
-// lemon.description = 'yummy lemon and salmon, together!';
-//
-// sugar.cookieName = 'salmon sugar cookie';
-// sugar.bake_date ='4/10/17';
-// sugar.price = 1.99;
-// sugar.ingredients = ['salmon','cinnamon','sugar'];
-// sugar.description = 'delicious salmon and cinnamon sweet cookies, yum!';
-
-//NOTE: I totally missed the assignment domain page at the bottom! starting over!!!!
 
 //1.
 var store_hours= ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
